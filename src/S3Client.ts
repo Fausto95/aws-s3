ae33ba80b1108affa9eb604ead25620045c8dce9
Wellington Vicioso <wellington.vicioso@baruchmail.cuny.edu>
2019-01-28 10:47:58 -0500

    Update S3Client.ts

--- src/S3Client.ts
+++ src/S3Client.ts
@@ -48,7 +48,7 @@ class S3Client {
     }
     public async deleteFile(fileName: string): Promise<DeleteResponse> {
       const url: string = `https://${this.config.bucketName}.s3${
-        this.config.region ? "-" + config.region : ""
+        this.config.region ? "-" + this.config.region : ""
         }.amazonaws.com/${
         this.config.dirName ? this.config.dirName + "/" : ""
         }${fileName}`;
