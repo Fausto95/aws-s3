03f10c297f736ff6bca890d0d6e7595a19703a76
victorct-pulsarup <44884350+victorct-pulsarup@users.noreply.github.com>
2019-02-27 12:37:37 -0600

    Fixing issue: #45 Invalid URL in region us-east-1
    Fixing issue: https://github.com/Fausto95/aws-s3/issues/45
    When the region is `us-east-1` the URL should be built in another way than the other regions (regions in China do not apply this rule)

--- src/Url.ts
+++ src/Url.ts
@@ -1,9 +1,10 @@
 
 export default (countryCode: string, bucketName: string, region: string): string => {
-  switch(countryCode) {
-    case "cn":
-      return `https://${bucketName}.s3.${region}.amazonaws.com.${countryCode}`;
-    default:
-      return `https://${bucketName}.s3-${region}.amazonaws.com`;
+  if(countryCode === "cn"){
+     return `https://${bucketName}.s3.${region}.amazonaws.com.${countryCode}`;
+  }else if(region === "us-east-1"){
+     return `https://${bucketName}.s3.${region}.amazonaws.com`;
+  }else{
+    return `https://${bucketName}.s3-${region}.amazonaws.com`;
   }
 }
